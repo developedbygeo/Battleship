1e2ea27b5ec973ee4c31d2a87c93c191692789dd
georgiosan <georgiosanagnostou@hotmail.com>
2021-11-04 21:02:39 +0200

    Adding the Ship class base file

---
+++ src/modules/Ship.js
@@ -0,0 +1,60 @@
+import _ from 'lodash';
+import calculateShipPosition from './helpers';
+
+export default class Ship {
+  #orientation;
+
+  #length;
+
+  #position;
+
+  #hits;
+
+  constructor(name, length) {
+    this.name = name;
+    this.#length = length;
+    this.#position = [];
+    this.#orientation = 'vertical';
+    this.#hits = [];
+  }
+
+  get length() {
+    return this.#length;
+  }
+
+  set orientation(newValue) {
+    this.#orientation = newValue;
+  }
+
+  get getOrientation() {
+    return this.#orientation;
+  }
+
+  set position(coord) {
+    // resetting any existing data & filling it with new data
+    this.#position = [];
+    calculateShipPosition(coord, this.#length, this.getOrientation).forEach(
+      (coordinate) => this.#position.push(coordinate)
+    );
+  }
+
+  get getPosition() {
+    return this.#position;
+  }
+
+  hit(coord) {
+    this.#hits.push(coord);
+  }
+
+  get getHits() {
+    return this.#hits;
+  }
+
+  isSunk() {
+    return Number(this.getHits.length) === Number(this.length);
+  }
+
+  reset() {
+    this.#hits.length = 0;
+  }
+}
