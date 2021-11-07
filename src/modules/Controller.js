79a3e2ad85df3deff809f303f37ce0fa19e6c78b
georgiosan <georgiosanagnostou@hotmail.com>
2021-11-07 04:00:45 +0200

    Added the findLoserParty, play and randomAI methods

--- src/modules/Controller.js
+++ src/modules/Controller.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import Player from './Player.js';
 
 export default class Controller {
@@ -6,6 +7,7 @@ export default class Controller {
     this.playerAi = new Player('AI');
     this.currentPlayers = [this.playerHuman, this.playerAi];
     this.turn = 0;
+    this.gameOver = false;
   }
 
   changeTurn() {
@@ -20,11 +22,38 @@ export default class Controller {
     return this.currentPlayers[this.turn];
   }
 
-  play() {
-    if (this.#checkTurn() === this.playerHuman) {
-      return this.playerHuman.name;
-    } else {
-      return this.playerAi.name;
+  findLoserParty() {
+    try {
+      return this.currentPlayers.find(
+        (player) => player.board.shipsAlive() === 0
+      ).name;
+    } catch {
+      throw new Error('Oops, no one has lost yet!');
     }
   }
+
+  play() {
+    if (this.#checkTurn() === this.playerHuman && this.gameOver === false) {
+      return this.playerHuman.name;
+    }
+    if (this.#checkTurn() === this.playerAi && this.gameOver === false) {
+      return this.playerAi.name;
+    }
+    return 'Game Over';
+  }
+
+  // AI logic
+  randomAI() {
+    let selection;
+    const availableCells = this.playerHuman.board.remainingCells();
+    if (availableCells.length === 1) {
+      // eslint-disable-next-line prefer-destructuring
+      selection = availableCells[0];
+    } else {
+      const randomSelection = _.random(0, availableCells.length);
+      selection = availableCells[randomSelection];
+    }
+    return selection;
+  }
+  // AiAttack(pastSelection, shipGuess = null){}
 }
