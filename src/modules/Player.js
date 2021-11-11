c9a18fd3559d16e4ad1c83e2ff7236742565f4b7
georgiosan <georgiosanagnostou@hotmail.com>
2021-11-11 21:30:38 +0200

    Started refactoring Player into a simpler class

--- src/modules/Player.js
+++ src/modules/Player.js
@@ -1,52 +1,31 @@
 /* eslint-disable class-methods-use-this */
-import _ from 'lodash';
-import Gameboard from './Gameboard.js';
+
 import Ship from './Ship.js';
 
 export default class Player {
-  constructor(name) {
+  constructor(name, myBoard, enemyBoard) {
     this.name = name;
+    this.myBoard = document.querySelector('.board-you');
+    this.enemyBoard = document.querySelector('.board-ai');
     this.fleet = [];
-    this.board = new Gameboard();
   }
 
-  createFleet() {
+  generateFleet() {
     const patrol = new Ship('patrol', 2);
     const cruiser1 = new Ship('cruiser1', 3);
     const cruiser2 = new Ship('cruiser2', 3);
     const battleship = new Ship('battleship', 4);
     const carrier = new Ship('carrier', 5);
     this.fleet = [patrol, cruiser1, cruiser2, battleship, carrier];
-    return [patrol, cruiser1, cruiser2, battleship, carrier];
   }
 
-  randomPlacement() {
-    const direction = ['vertical', 'horizontal'];
-    this.fleet.forEach((ship) => {
-      // specifies whether vertical or horizontal
-      const shipDirection = direction[_.random(0, 1)];
-      ship.orientation = shipDirection;
-      // random coord generator - checks if its valid
-      let randomCoord = _.random(0, 99);
-      while (
-        !this.board.isPositionValid(
-          randomCoord,
-          ship.length,
-          shipDirection,
-          false
-        )
-      ) {
-        randomCoord = _.random(0, 99);
-      }
-      this.board.placeShip(randomCoord, ship);
-    });
+  remainingTiles() {
+    return [...this.myBoard.querySelectorAll('.box')];
   }
 
-  attack(board, coord) {
-    board.shotFired(coord);
-  }
-
-  fleetReset() {
-    this.fleet.forEach((ship) => ship.hardReset());
+  checkHit(coord) {
+    const tile = document.querySelector(`#${coord}`);
+    if (tile.classList.contains('ship')) return true;
+    return false;
   }
 }
