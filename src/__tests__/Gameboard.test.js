51aec026d321d1529fdf154e01cc7c43a4fddcf4
georgiosan <georgiosanagnostou@hotmail.com>
2021-11-14 01:52:48 +0200

    Updating the tests

--- src/__tests__/Gameboard.test.js
+++ src/__tests__/Gameboard.test.js
@@ -1,111 +1,139 @@
 import Gameboard from '../modules/Gameboard.js';
 import Ship from '../modules/Ship.js';
 
-const testInstance = new Gameboard();
-const newCarrier = new Ship('cruiser', 3);
+let testInstance;
+const cruiser = new Ship('cruiser', 3);
+const invalidCruiser = new Ship('cruiser2', 3);
 
 describe('testing the Gameboard', () => {
+  testInstance = new Gameboard();
+  invalidCruiser.orientation = 'horizontal';
+  invalidCruiser.setPosition(8, 'horizontal');
   afterEach(() => {
-    testInstance.hardReset();
+    testInstance.reset();
+  });
+  it('testing the constructor', () => {
+    expect(testInstance.width).toBe(10);
+    expect(testInstance.area.length).toBe(100);
+    expect(testInstance.missedShots.length).toBe(0);
+    expect(testInstance.currentShips.length).toBe(0);
+    expect(testInstance.attackedCoords.length).toBe(0);
   });
 
-  it('#1 setting ship on currentShips', () => {
-    testInstance.setCurrentShips(newCarrier);
-    expect(testInstance.getCurrentShipsOnBoard.length).toBe(1);
+  it('placing ship - correctly', () => {
+    testInstance.placeShip(9, cruiser);
+    expect(testInstance.currentShips.length).toBe(1);
+    expect(testInstance.existingShips().length).toBe(1);
   });
 
-  it('#2 missedAttacks should be empty', () => {
-    expect(testInstance.missedShots).toEqual([]);
+  it('available cells', () => {
+    testInstance.placeShip(9, cruiser);
+    testInstance.handleAttack(95);
+    testInstance.handleAttack(92);
+    testInstance.handleAttack(91);
+    testInstance.handleAttack(85);
+    testInstance.handleAttack(1);
+    expect(testInstance.remainingUnoccupiedCells().length).toBe(95);
   });
 
-  it('#3 no ships on board', () => {
-    expect(testInstance.getCurrentShipsOnBoard).toEqual([]);
-  });
-
-  it('#4 should return true for valid position', () => {
-    expect(testInstance.isPositionValid(5, 3, 'horizontal')).toBe(true);
-  });
-
-  it('#5 should return false due to ship overlap', () => {
-    testInstance.placeShip(22, new Ship('carrier', 4));
-    expect(testInstance.isPositionValid(31, 3, 'horizontal')).toBe(false);
-  });
-
-  it('#6 should return false due to invalid position', () => {
-    expect(testInstance.isPositionValid(5, 99, 'horizontal')).toBe(false);
-  });
-
-  it('#7 should return false due to invalid - rotated position', () => {
-    expect(testInstance.isPositionValid(5, 99, 'vertical', true)).toBe(false);
-  });
-
-  it('#8 should return false due to valid - rotated position', () => {
-    expect(testInstance.isPositionValid(5, 11, 'horizontal', true)).toBe(true);
-  });
-
-  it('#9 invalid placeShip - return false', () => {
-    newCarrier.position(99, 'vertical');
-    expect(testInstance.placeShip(99, newCarrier)).toBe(false);
-  });
-
-  it('#10 valid placeShip - push to array', () => {
-    newCarrier.position(23, 'vertical');
-    testInstance.placeShip(23, newCarrier);
-    expect(testInstance.getCurrentShipsOnBoard.length).toBe(1);
-  });
-
-  it('#11 invalid rotateShip', () => {
-    newCarrier.position(48, 'vertical');
+  it('placing ship - incorrectly', () => {
     expect(() => {
-      testInstance.rotateShip(newCarrier);
-    }).toThrow('Invalid positioning');
+      testInstance.placeShip(8, invalidCruiser);
+    }).toThrow('Position is invalid');
   });
 
-  it('#12 valid rotateShip', () => {
-    newCarrier.position(35, 'vertical');
-    testInstance.rotateShip(newCarrier);
-    expect(newCarrier.getPosition[0]).toBe(35);
+  it('placing multiple ships - correctly', () => {
+    cruiser.setPosition(5, 'horizontal');
+    const cruiser3 = new Ship('cruiser3', 3);
+    cruiser3.setPosition(65, 'horizontal');
+    testInstance.placeShip(5, cruiser);
+    testInstance.placeShip(65, cruiser3);
+    expect(testInstance.currentShips).toContain(cruiser);
+    expect(testInstance.currentShips).toContain(cruiser3);
   });
 
-  it('#13 fired a duplicate shot to the same spot', () => {
-    newCarrier.position(35, 'horizontal');
-    testInstance.setCurrentShips(newCarrier);
-    testInstance.attackedCoords.push(35);
+  it('testing position - horizontal -  invalid', () => {
+    expect(testInstance.isPositionValid(9, 3, 'horizontal', true)).toBe(false);
+  });
+
+  it('testing position - vertical - valid', () => {
+    expect(testInstance.isPositionValid(9, 3, 'vertical', false)).toBe(true);
+  });
+
+  it('testing ship rotation - vertical - invalid', () => {
+    invalidCruiser.setPosition(99, 'horizontal');
     expect(() => {
-      testInstance.shotFired(35);
-    }).toThrow('A shot has already been fired here!');
+      testInstance.rotateShip(invalidCruiser);
+    }).toThrow(Error);
   });
 
-  it('#14 successfull shot, shotResult array is updated', () => {
-    newCarrier.position(35, 'horizontal');
-    testInstance.setCurrentShips(newCarrier);
-    expect(testInstance.shotFired(35).length).toEqual(1);
+  it('testing ship rotation - vertical - valid', () => {
+    cruiser.orientation = 'vertical';
+    cruiser.setPosition(55, 'vertical');
+    testInstance.rotateShip(cruiser);
+    expect(cruiser.orientation).toBe('vertical');
   });
 
-  it('#15 1 ship is alive and well', () => {
-    newCarrier.position(35, 'horizontal');
-    testInstance.setCurrentShips(newCarrier);
-    expect(testInstance.shipsAlive()).toBe(1);
+  it('testing ship rotation - vertical x2 - invalid', () => {
+    cruiser.orientation = 'vertical';
+    cruiser.setPosition(48, 'vertical');
+    expect(() => {
+      testInstance.rotateShip(cruiser);
+    }).toThrow(Error);
   });
 
-  it('#16 testing available cells - successfull query - cells taken', () => {
-    testInstance.attackedCoords = [0, 1, 2, 3, 4];
-    expect(testInstance.remainingCells().length).toBe(95);
+  it('testing ship rotation - horizontal - invalid', () => {
+    invalidCruiser.orientation = 'horizontal';
+    invalidCruiser.setPosition(99, 'horizontal');
+    expect(() => {
+      testInstance.rotateShip(invalidCruiser);
+    }).toThrow(Error);
   });
 
-  it('#17 testing available cells - successfull query - all cells available', () => {
-    testInstance.attackedCoords = [];
-    expect(testInstance.remainingCells().length).toBe(100);
+  it('testing attacks - invalid', () => {
+    testInstance.attackedCoords.push(58);
+    expect(() => {
+      testInstance.handleAttack(58);
+    }).toThrow(Error);
   });
 
-  it('#18 testing available cells - unsuccessfull query - all cells taken', () => {
-    testInstance.attackedCoords = [...Array(100).keys()];
-    expect(testInstance.remainingCells().length).toBe(0);
+  it('testing attacks - valid - no ship', () => {
+    testInstance.attackedCoords.push(55);
+    expect(testInstance.handleAttack(58).length).toBe(0);
   });
 
-  it('#19 valid placeShip - testing what is pushed', () => {
-    newCarrier.position(23, 'vertical');
-    testInstance.placeShip(23, newCarrier);
-    expect(testInstance.getCurrentShipsOnBoard[0].getPosition).toContain(23);
+  it('testing attacks - valid - ship exists', () => {
+    testInstance.attackedCoords.push(55);
+    cruiser.setPosition(9, 'vertical');
+    testInstance.setCurrentShips(cruiser);
+    expect(testInstance.handleAttack(19)).toContain(cruiser);
+    expect(cruiser.hits).toContain(19);
+    expect(testInstance.attackedCoords).toContain(19);
+  });
+
+  it('testing remainingShips - valid - ship exists', () => {
+    cruiser.setPosition(9, 'vertical');
+    testInstance.setCurrentShips(cruiser);
+    testInstance.handleAttack(19);
+    expect(testInstance.remainingShips()).toBe(1);
+  });
+
+  it('testing remainingShips - multiple - valid - ships exist', () => {
+    const cruiser3 = new Ship('cruiser3', 3);
+    cruiser.setPosition(9, 'vertical');
+    cruiser3.setPosition(65, 'horizontal');
+    testInstance.setCurrentShips(cruiser);
+    testInstance.setCurrentShips(cruiser3);
+    expect(testInstance.remainingShips()).toBe(2);
+  });
+
+  it('testing remainingShips - multiple - valid - ship exists and one is sunk', () => {
+    const cruiser3 = new Ship('cruiser3', 3);
+    cruiser.setPosition(9, 'vertical');
+    cruiser3.setPosition(65, 'horizontal');
+    cruiser3.hits = [65, 66, 67];
+    testInstance.setCurrentShips(cruiser);
+    testInstance.setCurrentShips(cruiser3);
+    expect(testInstance.remainingShips()).toBe(1);
   });
 });
