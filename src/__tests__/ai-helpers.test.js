ce06a54b37e9dfa3d4cccb914862587160c5da4d
georgiosan <georgiosanagnostou@hotmail.com>
2021-11-18 21:20:56 +0200

    Adding the tests for the ai helper functions

--- src/__tests__/ai-helpers.test.js
+++ src/__tests__/ai-helpers.test.js
@@ -1,71 +1,60 @@
-import * as AI from '../modules/ai-helpers.js';
+import { findValidXY, filterAttackedCells, findEnemyDirection } from '../modules/ai-helpers.js';
 
-describe('findValid tests', () => {
-  it('#1 testing base cases', () => {
-    expect(AI.findValid(0).length).toBe(2);
-    expect(AI.findValid(9).length).toBe(2);
-    expect(AI.findValid(90).length).toBe(2);
-    expect(AI.findValid(99).length).toBe(2);
-    // defaults
-    expect(AI.findValid(34)).toContain(33);
-    expect(AI.findValid(34)).toContain(35);
-    expect(AI.findValid(34)).toContain(44);
-    expect(AI.findValid(34)).toContain(24);
+describe('testing findValidXY', () => {
+  it('testing findValidXY - no direction arg', () => {
+    expect(findValidXY(5)).toBe('nope');
   });
 
-  it('#2 testing num included', () => {
-    expect(AI.findValid(92).length).toBe(3);
-    expect(AI.findValid(92)).toContain(93);
-    expect(AI.findValid(29).length).toBe(3);
-    expect(AI.findValid(29)).toContain(19);
-    expect(AI.findValid(7).length).toBe(3);
-    expect(AI.findValid(7)).toContain(17);
-    expect(AI.findValid(60).length).toBe(3);
-    expect(AI.findValid(60)).toContain(61);
-  });
-});
-
-describe('findValidHorizontal tests', () => {
-  it('#3 testing base cases', () => {
-    expect(AI.findValidHorizontal(0)).toBe('nope');
-    expect(AI.findValidHorizontal(9)).toBe('nope');
-    expect(AI.findValidHorizontal(90)).toBe('nope');
-    expect(AI.findValidHorizontal(99)).toBe('nope');
-    expect(AI.findValidHorizontal(29)).toBe('nope');
-    expect(AI.findValidHorizontal(70)).toBe('nope');
-    // defaults
-    expect(AI.findValidHorizontal(34)).toContain(33);
-    expect(AI.findValidHorizontal(34)).toContain(35);
+  it('testing findValidXY - horizontal - valid', () => {
+    expect(findValidXY(5, 'horizontal')).toContain(4);
+    expect(findValidXY(5, 'horizontal')).toContain(6);
+    expect(findValidXY(5, 'horizontal').length).toBe(2);
   });
 
-  it('#4 testing num included', () => {
-    expect(AI.findValidHorizontal(8)).toContain(9);
-    expect(AI.findValidHorizontal(8)).toContain(7);
-    expect(AI.findValidHorizontal(93)).toContain(94);
-    expect(AI.findValidHorizontal(93)).toContain(92);
+  it('testing findValidXY - horizontal - nope', () => {
+    expect(findValidXY(90, 'horizontal')).toBe('nope');
+  });
+
+  it('testing findValidXY - vertical - valid', () => {
+    expect(findValidXY(45, 'vertical')).toContain(55);
+    expect(findValidXY(45, 'vertical')).toContain(35);
+    expect(findValidXY(45, 'vertical').length).toBe(2);
+    expect(findValidXY(29, 'vertical')).toContain(19);
+    expect(findValidXY(70, 'vertical')).toContain(80);
+  });
+
+  it('testing findValidXY - vertical - nope', () => {
+    expect(findValidXY(8, 'vertical')).toBe('nope');
+  });
+
+  it('testing findValidXY - unavailable - valid', () => {
+    expect(findValidXY(0, 'unavailable')).toContain(10);
+    expect(findValidXY(9, 'unavailable')).toContain(8);
+    expect(findValidXY(90, 'unavailable')).toContain(80);
+    expect(findValidXY(99, 'unavailable')).toContain(98);
+    expect(findValidXY(20, 'unavailable')).toContain(30);
+    expect(findValidXY(39, 'unavailable')).toContain(29);
+    expect(findValidXY(92, 'unavailable')).toContain(82);
+    expect(findValidXY(8, 'unavailable')).toContain(18);
+    expect(findValidXY(45, 'unavailable')).toContain(46);
   });
 });
 
-describe('findValidVertical tests', () => {
-  it('#5 testing base cases', () => {
-    expect(AI.findValidVertical(0)).toBe('nope');
-    expect(AI.findValidVertical(9)).toBe('nope');
-    expect(AI.findValidVertical(90)).toBe('nope');
-    expect(AI.findValidVertical(99)).toBe('nope');
-    expect(AI.findValidVertical(5)).toBe('nope');
-    expect(AI.findValidVertical(98)).toBe('nope');
-    // defaults
-    expect(AI.findValidVertical(44)).toContain(34);
-    expect(AI.findValidVertical(44)).toContain(54);
+describe('testing filterAttackedCells', () => {
+  it('testing filterAttackedCells', () => {
+    const valid = [0, 5, 25, 35, 99, 56];
+    const attacked = [0, 99, 13, 55, 21, 25, 56];
+    expect(filterAttackedCells(valid, attacked)).toContain(35);
+    expect(filterAttackedCells(valid, attacked)).toContain(5);
+    expect(filterAttackedCells(valid, attacked).length).toBe(2);
   });
+});
 
-  describe('Testing findEnemyDirection', () => {
-    it('testing vertical', () => {
-      expect(AI.findEnemyDirection([5, 15])).toBe('vertical');
-    });
-
-    it('testing horizontal', () => {
-      expect(AI.findEnemyDirection([5, 6])).toBe('horizontal');
-    });
+describe('testing findEnemyDirection', () => {
+  it('testing findEnemyDirection', () => {
+    const attacks = [0, 1];
+    const attacks2 = [89, 99];
+    expect(findEnemyDirection(attacks)).toBe('horizontal');
+    expect(findEnemyDirection(attacks2)).toBe('vertical');
   });
 });
