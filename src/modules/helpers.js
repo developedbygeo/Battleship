b018edc06e5972815063a838691895da14fc7366
georgiosan <georgiosanagnostou@hotmail.com>
2021-11-24 23:52:00 +0200

    Code cleanup

--- src/modules/helpers.js
+++ src/modules/helpers.js
@@ -6,8 +6,8 @@ export default function determinePosition(originalCoord, length, orientation) {
       finalPosition[i] = finalPosition[i - 1] + 10;
     }
   } else {
-    for (let j = 1; j < length; j += 1) {
-      finalPosition[j] = finalPosition[j - 1] + 1;
+    for (let i = 1; i < length; i += 1) {
+      finalPosition[i] = finalPosition[i - 1] + 1;
     }
   }
   return finalPosition;
